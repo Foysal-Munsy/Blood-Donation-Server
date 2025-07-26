a87958b392b89cdcbf328502212c64f37af80f1b
Foysal-Munsy <bdfoysal555@gmail.com>
2025-07-26 18:33:48 +0600

    Server Setup

---
+++ index.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const cors = require("cors");
+
+const {
+  MongoClient,
+  ServerApiVersion,
+  ObjectId,
+  ChangeStream,
+} = require("mongodb");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const admin = require("firebase-admin");
+
+const serviceAccount = require("./admin-key.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// MongoDB Connection
+const client = new MongoClient(process.env.MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+// middleware/auth.js
+
+const verifyFirebaseToken = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ message: "Unauthorized: No token provided" });
+  }
+
+  const idToken = authHeader.split(" ")[1];
+
+  try {
+    const decodedToken = await admin.auth().verifyIdToken(idToken);
+    req.firebaseUser = decodedToken; // You can access user info like uid, email, etc.
+    next();
+  } catch (error) {
+    return res
+      .status(401)
+      .json({ message: "Unauthorized: Invalid token from catch" });
+  }
+};
+
+async function run() {
+  try {
+    await client.connect();
+    const db = client.db("blood_donation");
+    const events = db.collection("collection1");
+  } finally {
+  }
+}
+
+run().catch(console.dir);
+
+// Root route
+
+app.get("/", verifyFirebaseToken, async (req, res) => {
+  console.log(req.firebaseUser);
+
+  res.send("Server is running!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
